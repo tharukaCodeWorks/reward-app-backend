a7c78c5bf0522322b6f1c6ada2d0de757ddd71db
Tharuka Lakshan <tharukawapnet@gmail.com>
2019-05-13 12:25:38 +0530

    Email Client

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'admin@teachmeit.lk'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@srireward.com'),
         'name' => env('MAIL_FROM_NAME', 'SriRewards'),
     ],
 
